2c082c8a80da2795026abb025c9aca9bb22d5157
ajay-tac-adobe <tacadobe4@gmail.com>
2026-01-05 14:09:59 +0530

    added placeholder, banner and sections

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,6 +1,8 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
+import { fetchPlaceholders } from '../../scripts/placeholders.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
+  const placeholders = await fetchPlaceholders();
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
@@ -8,7 +10,16 @@ export default function decorate(block) {
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      else {
+        div.className = 'cards-card-body';
+        // Add placeholder link
+        const p = document.createElement('p');
+        const link = document.createElement('a');
+        link.href = '#';
+        link.textContent = placeholders['click-here-more'] || 'Click here for more';
+        p.append(link);
+        div.append(p);
+      }
     });
     ul.append(li);
   });
